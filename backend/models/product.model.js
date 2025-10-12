0a6c9bb10b10aaa3ceee8f07824c5705a412d553
ifeoluwa <youremail@example.com>
2025-10-12 13:22:12 +0100

    addition of comments

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,18 +1,23 @@
 import mongoose from 'mongoose';
 
+// Product Schema (form or required details to create a product)
+
 const productSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
     startingPrice: { type: Number, required: true },
     currentPrice: { type: Number, default: 0 },
     image: {type: String, required: true },  // Array of image URLs
-    Timestamps: { type: Date, default: Date.now },
     seller: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     bids: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bid" }],
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true },
     status: { type: String, enum: ["active", "closed"], default: "active" }
-});
+},
+
+{ timestamps: true },
+
+);
 
 const Product = mongoose.model("Product", productSchema);
 
