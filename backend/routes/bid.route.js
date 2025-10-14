09b98c65a91da91fe377754484627d8cc6422156
ifeoluwa <youremail@example.com>
2025-10-14 19:17:14 +0100

    bid routes

---
+++ backend/routes/bid.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
+import { 
+    getUserBids, 
+    deleteBid,
+    placeBid,
+    getBidsForProduct
+
+} from "../controllers/bid.controller.js";
+
+const router = express.Router();
+
+// Get all bids for the logged-in user
+router.get("/my-bids", protect, getUserBids);
+router.delete("/:bidId", protect, deleteBid);
+router.post("/:id/bid", protect, placeBid);
+router.post("/:id/bid", protect, getBidsForProduct);
+
+export default router;
